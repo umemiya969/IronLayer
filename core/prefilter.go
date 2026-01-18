55b8cb232e2863235103fde962ec839765465696
umemiya969 <elanie477@gmail.com>
2026-01-19 05:17:44 +0800

    Refactor code structure for improved readability and maintainability

---
+++ core/prefilter.go
@@ -0,0 +1,35 @@
+package core
+
+import (
+	"net"
+	"strings"
+)
+
+func PreFilter(ctx *RequestContext, cfg *Config) {
+
+	// block empty IP
+	if ctx.IP == "" {
+		ctx.Score += 20
+		return
+	}
+
+	// block localhost
+	if ctx.IP == "127.0.0.1" || ctx.IP == "::1" {
+		ctx.Score += 20
+		return
+	}
+
+	// block private network
+	ip := net.ParseIP(ctx.IP)
+	if ip != nil {
+		if ip.IsPrivate() {
+			ctx.Score += 20
+			return
+		}
+	}
+
+	// suspicious UA
+	if strings.TrimSpace(ctx.UA) == "" {
+		ctx.Score += 3
+	}
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module ironlayer
 
 go 1.22
 
-require (
-	gopkg.in/yaml.v3 v3.0.1
-)
+require gopkg.in/yaml.v3 v3.0.1
